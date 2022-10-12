55c988f37a88714271a98ee1483d0d176fc4f5e1
33011-31-02 <33011-31-02@exchange.ad.afpanet>
2022-10-12 15:42:47 +0200

    premiere modif

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php
+// indiqué le chemin de votre fichier JSON, il peut s'agir d'une URL
+
+$fileNane="data.json";
+$article=array();
+if (file_exists ($fileName)) {
+    
+}
+$data = file_get_contents("$fileName");
+
+
+var_dump(json_decode($json));
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,17 +23,46 @@
 
 <body>
     <?php require_once './includes/header.php' ?>
+
+    <!-- liste cards -->
     <div class="container">
-
-        <div class="content">
-            <?php
-// indiqué le chemin de votre fichier JSON, il peut s'agir d'une URL
-$json = file_get_contents("data.json");
-
-var_dump(json_decode($json));
-?>
-            Content
+        <div class="row align-items-center justify-content-center-between">
+            <!--  card-01 -->
+            <div class="col-md-4 text-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="assets/img/gallery-10.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Titre</h5>
+                        <a href="#" class="btn btn-primary">détail</a>
+                    </div>
+                </div>
+            </div>
+            <!--  card-02 -->
+            <div class="col-md-4 text-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="assets/img/gallery-10.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Titre</h5>
+                        <a href="#" class="btn btn-primary">détail</a>
+                    </div>
+                </div>
+            </div>
+            <!--  card-03 -->
+            <div class="col-md-4 text-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="assets/img/gallery-10.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Titre</h5>
+                        <a href="#" class="btn btn-primary">détail</a>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>
+
+    <!--     fin card -->
+    </div>
+
     </div>
 </body>
 
